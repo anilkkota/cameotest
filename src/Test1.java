95fd9c41ce5463a80da1c4f098334ae65345ad89
Anil <anilk501@gmail.com>
2015-07-08 23:53:53 +0530

    first commit

---
+++ src/Test1.java
@@ -0,0 +1,8 @@
+
+public class Test1 {
+
+	public void funtiona1()
+	{
+		System.out.println("Funtion 1");
+	}
+}
---
+++ src/TestMain.java
@@ -0,0 +1,14 @@
+
+public class TestMain {
+	
+	public static void main(String args[])
+	{
+		System.out.println("Sample Java class");
+		
+		System.out.println("Calling funtion 1");
+		
+		Test1 t1 = new Test1();
+		t1.funtiona1();
+	}
+
+}
